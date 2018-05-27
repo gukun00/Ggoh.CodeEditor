2a15db663bf0835150f7ba23d24c7dc94a87a2cc
gukun <gukun@gukundeMacBook-Pro.local>
2018-05-27 17:04:51 +0800

    尝试发布成功

--- codeeditor/webpack.config.js
+++ codeeditor/webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
-    filename: 'vue-codeeditor.js', //打包生成文件的名字
+    filename: 'build.js', //打包生成文件的名字
     library: 'CodeEditor', //reqire引入的名字
     libraryTarget: 'umd',
     umdNamedDefine: true
